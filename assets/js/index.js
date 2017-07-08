1b27b2ece08686dfef823890fc912cc3d38c1122
Artem Maksimov <temamaksimov@gmail.com>
2017-07-08 15:53:15 +0300

    Huge update

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,6 +11,24 @@ function showModal(id, title_text) {
     backdrop.style.display = 'block';
     title.innerText = title_text;
     image.src = "assets/images/photo-" + id + ".jpg"
+    image.srcset = generateSRCSET(id);
+}
+
+function generateSRCSET(id) {
+    var srcset_string = "";
+    var resolutions = ['320', '640', '750', '828', '1440', '1536', '2048', '3840'];
+    for(const i in resolutions) {
+        const w = resolutions[i];
+        srcset_string += "assets/images/photo-" + id + "@" + w + "w.jpg " + w + "w";
+
+        console.log(i + ' ' + (resolutions.length - 1));
+        if(parseInt(i) === resolutions.length - 1) {
+            srcset_string += ''
+        } else {
+            srcset_string += ', '
+        }
+    }
+    return srcset_string;
 }
 
 function hideModal() {
@@ -32,4 +50,34 @@ document.addEventListener('DOMContentLoaded', function(){
         hideModal();
     },false);
 
+    // object fit fallback
+
+    // If object fit supported
+    if('objectFit' in document.documentElement.style === false) {
+
+        // get all image items
+        var items = document.querySelectorAll('.item');
+
+        // Iterate through image items
+        for(var i = 0; i < items.length; i++) {
+            console.log(items[i]);
+
+            if(!items[i].querySelector('img'))
+                continue;
+            // Get image source
+            var imageSource = items[i].querySelector('a').href;
+
+            // Hide image
+            items[i].querySelector('a').style.display = 'none';
+
+            // Add background-size: cover
+            items[i].style.backgroundSize = 'cover';
+
+            // Add background-image: and put image source here
+            items[i].style.backgroundImage = 'url(' + imageSource + ')';
+
+            // Add background-position: center center
+            items[i].style.backgroundPosition = 'center center';
+        }
+    }
 });
